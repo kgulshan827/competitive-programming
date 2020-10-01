e32da641e1a68645727c9313b950aaefffe7f4aa
kgulshan827 <kgulshan827@gmail.com>
2020-10-01 20:04:12 +0530

    added linked list

---
+++ linkedlist/reverselinkedlist.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+  public:
+    int data;
+    Node* next;
+  
+};
+void printList(Node* n) 
+{ 
+    while (n != NULL) { 
+        cout << n->data << " "; 
+        n = n->next; 
+    } 
+} 
+
+
+
+
+ void reverse(Node **head)
+{
+  Node *prev=NULL,*current=*head,*next=NULL,*newhead=NULL;
+  while(current!=NULL)
+  {
+    next=current->next;
+    current->next=prev;
+    prev=current;
+    current=next;
+  }
+  *head=prev;
+  
+}
+
+int main() {
+  // your code goes here  
+  ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    Node* head=NULL;
+    Node* head1=NULL;
+    Node* head2=NULL;
+    head=new Node();
+    head1=new Node();
+    head2=new Node();
+    head->data=2;
+    head->next=head1;
+    head1->data=3;
+    head1->next=head2;
+    head2->data=2;
+    head2->next=NULL;
+
+    printList(head);
+    dupli(&head);
+    printList(head);
+
+  }
+
