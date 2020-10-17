cc8d6738b8b8d5986ce93a50b846cee8336dadf0
gkc827 <72963077+gkc827@users.noreply.github.com>
2020-10-17 20:14:23 +0530

    Update Detectloop.cpp

--- linkedlist/Detectloop.cpp
+++ linkedlist/Detectloop.cpp
@@ -1,3 +1,24 @@
 
 Question:
 Given a linked list, check if the linked list has loop or not. Below diagram shows a linked list with a loop. 
+  
+  
+
+  
+Solution:
+
+This contains a detecloop function which takes head pointer as input and return true or false if there is a loop or not.
+  
+  bool hasCycle(ListNode *head) {
+        ListNode *slow=head,*fast=head;
+        while(fast!=NULL && fast->next!=NULL && fast->next->next!=NULL && slow!=NULL && slow->next!=NULL)
+        {
+            slow=slow->next;
+            fast=fast->next->next;
+            if(slow==fast)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
