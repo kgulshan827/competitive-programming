a5f749553f89bea04b08685f1d5a874018a04552
kgulshan827 <56105750+kgulshan827@users.noreply.github.com>
2020-10-28 12:02:30 +0530

    Update Addtwonumber.cpp

--- linkedlist/Addtwonumber.cpp
+++ linkedlist/Addtwonumber.cpp
@@ -3,7 +3,12 @@ Question:
 
 Given two numbers represented by two linked lists of size N and M. The task is to return a sum list. The sum list is a linked list representation of the addition of two input numbers.
 
-
+Example:
+Input:
+3-4-5
+4-5
+Output:
+3-9-0
 Solution:
 Node* newNode(int data) 
 { 
